bc7f798a826bfde6e1a1ab75e476449496a5c948
Tom <tomdesvignes031@gmail.com>
2023-03-23 09:38:11 +0100

    maj

--- src/component/CartContext.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { createContext } from "react";
-
-const CartContext = createContext ([])
-
-export default function CartContextProvider ({children}){
-    const [myCart, setMyCart] = useState 
-    return <CartContextProvider
-}
