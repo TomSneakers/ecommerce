e9bf3f84e75d1a85f5c9946ab5e36f8b2371ee4b
Tom <tomdesvignes031@gmail.com>
2023-03-24 09:19:53 +0100

    maj

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { createContext } from "react";
 
-export const CartContext = createContext([])
+export const CartContext = createContext([]);
 
 // Composant
-export default function CartContextProvider({children}) {
+export default function CartContextProvider({ children }) {
+  // remplacer ce hook par le hook useReducer
+  const [myCart, setMyCart] = useState([]);
 
-    // remplacer ce hook par le hook useReducer
-    const [myCart, setMyCart] = useState([])
-
-    return <CartContext.Provider value={{myCart, setMyCart}}>
-            {children}
+  return (
+    <CartContext.Provider value={{ myCart, setMyCart }}>
+      {children}
     </CartContext.Provider>
+  );
 }
--- src/views/panier.jsx
+++ src/views/panier.jsx
@@ -1,3 +1,9 @@
+import { CartContext } from "./context/CartContext";
+const myCartContext = useContext(CartContext);
+
 export default function Panier() {
+  useEffect(() => {
+    console.log(myCartContext);
+  }, []);
   return <div>Mon panier</div>;
 }
